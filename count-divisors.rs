b4b59fc747eaaad2e13f2778cdeb46f23a09f7ba
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2016-12-19 02:08:28 -0800

    Add a condition to avoid mod operation for every iteration

--- count-divisors.rs
+++ count-divisors.rs
@@ -4,11 +4,10 @@ fn main(){
     let v : Vec<u32> = input.split_whitespace().map(|_str| _str.parse::<u32>().unwrap_or(0)).collect();
     let mut cnt = 0;
     for i in v[0]..v[1]+1{
-        if i % v[2] == 0{
+        if v[2] <= i && i % v[2] == 0{
             cnt += 1;
         }
     }
 
-    println!("{}", cnt);
-
+    println!("{}", cnt); 
 }
