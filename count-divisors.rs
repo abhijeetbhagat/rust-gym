9a87687c16b7e3983f57fa869fe73fb8ae691f2f
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2016-12-19 01:34:00 -0800

    Count divisors in a given range

---
+++ count-divisors.rs
@@ -0,0 +1,14 @@
+fn main(){
+    let mut input= String::new();
+    std::io::stdin().read_line(&mut input);
+    let v : Vec<u32> = input.split_whitespace().map(|_str| _str.parse::<u32>().unwrap_or(0)).collect();
+    let mut cnt = 0;
+    for i in v[0]..v[1]+1{
+        if i % v[2] == 0{
+            cnt += 1;
+        }
+    }
+
+    println!("{}", cnt);
+
+}
